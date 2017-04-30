ce5198637d38d14271761991eb8772aac437223a
Mopsalarm <mopsalarm@users.noreply.github.com>
2017-04-30 10:05:15 +0200

    Fixes.

--- run-vultr.sh
+++ run-vultr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -f vault.key ] ; then
-  exec ansible-playbook -i hosts-vultr --vault-password-file=vault.key "$@"
+  exec ./ansiblew -i hosts-vultr --vault-password-file=vault.key "$@"
 else
-  exec ansible-playbook -i hosts-vultr --ask-vault-password "$@"
+  exec ./ansiblew -i hosts-vultr --ask-vault-pass "$@"
 fi
