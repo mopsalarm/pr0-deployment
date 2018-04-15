f66caa9cfc365e3145f244e481a7a4b0b4cb14c1
Mopsalarm <mopsalarm@users.noreply.github.com>
2018-04-15 11:49:40 +0200

    Update certbot

--- files/refresh-ssl-certificates.sh
+++ files/refresh-ssl-certificates.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-docker pull certbot/certbot:v0.19.0
+docker pull certbot/certbot:v0.23.0
 docker rm -f letsencrypt || true
 
 for DOMAIN in pr0.wibbly-wobbly.de app.pr0gramm.com ; do
@@ -9,11 +9,12 @@ for DOMAIN in pr0.wibbly-wobbly.de app.pr0gramm.com ; do
     -v "/data/nginx/static:/webroot" \
     -v "/data/nginx/ssl:/etc/letsencrypt" \
     -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-    certbot/certbot:v0.19.0 \
+    certbot/certbot:v0.23.0 \
       auth -d $DOMAIN \
       --webroot --webroot-path /webroot \
       -m mopsalarm.pr0gramm@gmx.de \
-      --renew-by-default --agree-tos
+      --renew-by-default --agree-tos \
+      --non-interactive
 done
 
 echo "reloading nginx"
